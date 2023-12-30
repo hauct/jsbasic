29d5308abdf9386b6e04ebe4d48ef017eb2eb728
hauct <cungtronghau@gmail.com>
2023-12-30 18:21:28 +0700

    edit something

--- video5/video5.js
+++
@@ -1,3 +0,0 @@
-console.log('Hello world from HTML')
-let a = '1$2$3$4$5'
-
---
+++ video6/video6.js
@@ -0,0 +1,18 @@
+console.log('Hello world from HTML'.toUpperCase())
+
+let a = 5
+let b = 6
+let c = 6.9
+let d = '5'
+
+// let e = Number('5')
+let e = +d
+
+console.log(a+b)
+console.log(a+c)
+
+console.log(a-d)
+
+console.log(a === d, 'a: ', typeof a, 'd: ', typeof d)
+
+console.log('e value', e, 'type:', typeof e)
