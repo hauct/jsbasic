bde7d93a3495516d6440244093f9666bd2d0be6d
hauct <cungtronghau@gmail.com>
2023-12-30 23:09:28 +0700

    edit something

---
+++ video9/video9.js
@@ -0,0 +1,14 @@
+console.log('Hello world from HTML');
+
+let a ={
+    name:'Hau',
+    age:'27'
+};
+
+console.log('object: ', a['name']);
+
+let b = ['MU', 'Liver', 'Chelsea'];
+
+console.log(`type of b: `, typeof b, b);
+console.log(`first item : `, b[0], `second item:`, b[1]);
+
