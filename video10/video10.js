7f460834b1cf3bcd971c28e0104ae4c4ad7e9b97
hauct <cungtronghau@gmail.com>
2023-12-31 21:08:34 +0700

    edit something

--- video10/video10.js
+++
@@ -1,6 +0,0 @@
-console.log('Hello word from HTML')
-
-let a=5;
-let b=5;
-
-console.log(`compare a&b: `, `a= `, a, `b= `, b,  )
---
+++ video11/video11.js
@@ -0,0 +1,13 @@
+console.log('Hello word from HTML')
+
+// for (let i = 1; i <= 10; i++) {
+//     console.log('check variables i:', i)
+// }
+
+let arr = ['Mu', 'Mancity', 'Liver', 'Chelsea', 'Notting']
+
+for (let i = 0; i < arr.length; i++) {
+    console.log('check variables i:', arr[i])
+}
+
+console.log('check variables i:', i)
