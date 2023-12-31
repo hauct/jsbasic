c870a0540dd219bb8a048b30a039a040cc7e4567
hauct <cungtronghau@gmail.com>
2023-12-31 22:09:35 +0700

    edit something

---
+++ video12/video12.js
@@ -0,0 +1,17 @@
+console.log('Hello word from HTML')
+let arrTop4 = ['Mancity', 'Liver', 'Mu', 'Chelsea'] 
+
+// for (let i = 0; i < arrTop4.length; i++) {
+//     console.log('Top ', i+1, arrTop4[i])
+// }
+
+var i = 0
+
+let flag=false
+
+while (!flag){
+    console.log('check value i: ', i);
+    i++;
+    if (i==10) flag = true;
+}
+
